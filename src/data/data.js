b2ea79bbe3ca2f74cb0c5afca7b155733f331743
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2023-12-04 12:11:55 +0700

    feat: portfolio

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Nike from '../assets/projects/nike.PNG'
+import Raven from '../assets/projects/raven.PNG'
+import Translate from '../assets/projects/translate.PNG'
+import App1 from '../assets/projects/app-1.png'
+import App2 from '../assets/projects/app-2.png'
+import App3 from '../assets/projects/app-3.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Next JS Landing Page",
+        image:Raven,
+        github:"https://github.com/buiminh15/next_raven_digital",
+        live:"https://next-raven-digital.vercel.app/",
+    },
+    {
+        id:2,
+        name:"Next JS Translation Application",
+        image:Translate,
+        github:"https://github.com/buiminh15/nextjs_translate_doc",
+        live:"https://nextjs-translate-doc.vercel.app/",
+    },
+    {
+        id:3,
+        name:"Next JS Landing Page",
+        image:Nike,
+        github:"https://github.com/buiminh15/nextjs-nike-webapp",
+        live:"https://nextjs-nike-webapp.vercel.app/",
+    },
+    {
+        id:4,
+        name:"React Native Application",
+        image:App1,
+        github:"https://github.com/buiminh15/RN-travel-booking-app-UI",
+        live:"https://github.com/buiminh15/RN-travel-booking-app-UI",
+    },
+    {
+        id:5,
+        name:"React Native Application",
+        image:App2,
+        github:"https://github.com/buiminh15/RN_travel_app",
+        live: "https://github.com/buiminh15/RN_travel_app",
+    },
+    {
+        id:6,
+        name:"React Native Application",
+        image:App3,
+        github:"https://github.com/buiminh15/RN_UI_book_hotel",
+        live: "https://github.com/buiminh15/RN_UI_book_hotel",
+    },
+]
